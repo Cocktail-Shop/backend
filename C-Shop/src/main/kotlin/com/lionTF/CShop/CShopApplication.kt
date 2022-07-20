c3a7d4197ac17e42865b01cd0889e64c58b109ca
이종수 <81428357+Lee-JongSoo@users.noreply.github.com>
2022-07-20 11:24:35 +0900

    프로젝트 build 파일 생성
    pull 하셔서 사용하시면 됩니디. application.yml 파일에 DB 연결 정보만 수정하셔서 사용하에요!

---
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/CShopApplication.kt
@@ -0,0 +1,11 @@
+package com.lionTF.CShop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<CShopApplication>(*args)
+}
---
+++ C-Shop/src/test/kotlin/com/lionTF/CShop/CShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lionTF.CShop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
