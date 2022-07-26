4c4c4f60bc1abb583328f460aabbf7e1bc9ec2fa
오경은 <47470474+KyungeunOh@users.noreply.github.com>
2022-07-26 09:48:14 +0900

    Update ItemService.kt
    CocktailRepository 지웠습니다

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/service/ItemService.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/service/ItemService.kt
@@ -2,7 +2,6 @@ package com.lionTF.CShop.domain.shop.service
 
 import com.lionTF.CShop.domain.shop.controller.dto.ReadItemDTO
 import com.lionTF.CShop.domain.shop.repository.ItemRepository
-import com.lionTF.CShop.domain.shop.repository.CocktailRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
