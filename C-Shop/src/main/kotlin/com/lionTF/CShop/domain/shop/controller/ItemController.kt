4b282ce934181553f196f2b27a0de689b0db97d4
채경준 <78475838+chae001122@users.noreply.github.com>
2022-07-28 08:48:17 +0900

    사용되지 않은 model을 모두 제거하였습니다.

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/ItemController.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/ItemController.kt
@@ -18,16 +18,13 @@ class ItemController (
 
     // item전체 조회. 테스트를 위한 용도임.
     @GetMapping(path=["/allItem"])
-    fun getAllItems(model: Model) : List<ReadItemDTO>{
-        val list: List<ReadItemDTO> = itemService.findAllItem()
-        model.addAttribute("itemList", list)
+    fun getAllItems() : List<ReadItemDTO>{
         return itemService.findAllItem()
     }
 
     //상품 단건 조회
     @GetMapping(path=["/items/{itemId}"])
-    fun getItemById(model: Model, @PathVariable("itemId") itemId: Long): ReadItemResultDTO {
-        model.addAttribute("item",itemService.findByItemId(itemId))
+    fun getItemById(@PathVariable("itemId") itemId: Long): ReadItemResultDTO {
         return itemService.findByItemId(itemId)
     }
 
