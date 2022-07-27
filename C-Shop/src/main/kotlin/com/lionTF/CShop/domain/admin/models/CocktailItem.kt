7b92bb6894abe18b061a0b7a078c9e90f57cea36
chae001122 <chae001122@daum.net>
2022-07-27 14:33:10 +0900

    Feat: 칵테일 단건 조회시 필요한 dto변환 메소드 추가
    toReadCocktailItemInfoDTO 메소드와 차이는 없지만, cocktail에서 직접적으로 item에 접근할 수 없기에 필요한 메소드입니다.

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/models/CocktailItem.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/models/CocktailItem.kt
@@ -1,5 +1,7 @@
 package com.lionTF.CShop.domain.admin.models
 
+import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailItemDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailItemInfoDTO
 import lombok.*
 import javax.persistence.*
 
@@ -23,4 +25,14 @@ class CocktailItem (
     private var cocktail: Cocktail,
 ) {
 
+    //칵테일 단건 조회시, cocktailItems에 들어갈 아이템 정보들을 dto로 변환해주는 메소드
+    fun toReadCocktailItemDTO(): ReadCocktailItemDTO {
+        val item = item.toReadCocktailItemInfoDTO()
+        return ReadCocktailItemDTO(
+            itemId = item.itemId,
+            itemName = item.itemName,
+            price = item.price,
+            amount = item.amount,
+        )
+    }
 }
