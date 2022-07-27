26b0d55379af63b346297656b99e033f1ee15a1b
chae001122 <chae001122@daum.net>
2022-07-27 14:29:43 +0900

    Feat: 칵테일 entity 필드 추가 및 dto 변환 메소드 추가
    cocktailDescription필드가 빠져있어서 추가하였습니다.
    칵테일 단건 조회를 위해 dto로 변환 해주는 메소드들을 추가하였습니다.

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/models/Cocktail.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/models/Cocktail.kt
@@ -1,5 +1,11 @@
 package com.lionTF.CShop.domain.admin.models
 
+import com.fasterxml.jackson.databind.BeanDescription
+import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailItemDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailResultDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ReadItemDTO
+import com.lionTF.CShop.global.HttpStatus
 import lombok.*
 import javax.persistence.*
 
@@ -16,8 +22,31 @@ class Cocktail (
 
     @OneToMany(mappedBy = "cocktail")
     private var cocktailItem: List<CocktailItem>,
-
+    private var cocktailDescription: String,
     private var cocktailName: String,
     private var cocktailImgUrl: String,
     private var cocktailStatus: Boolean,
-)
+){
+
+    //칵테일 단건 조회 응답 최종 형태 dto를 만들어주는 메소드
+    fun toReadCocktailResultDTO(result: ReadCocktailDTO): ReadCocktailResultDTO {
+        return ReadCocktailResultDTO(
+            status = HttpStatus.OK,
+            message = "칵테일 단건 조회 성공",
+            result = result
+        )
+    }
+
+    //칵테일 단건 조회 result부분에 들어갈 정보들을 dto로 변환
+    fun toReadCocktailDTO(): ReadCocktailDTO {
+        return ReadCocktailDTO(
+            cocktailId = cocktailId,
+            cocktailName = cocktailName,
+            cocktailDescription = cocktailDescription,
+            cocktailImgUrl = cocktailImgUrl,
+            cocktailStatus = cocktailStatus,
+            cocktailItems = cocktailItem.map{it.toReadCocktailItemDTO()}
+
+        )
+    }
+}
