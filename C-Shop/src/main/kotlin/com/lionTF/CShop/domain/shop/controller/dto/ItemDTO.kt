15418475991211c18d50c6ace39aa934d54632bf
chae001122 <chae001122@daum.net>
2022-07-27 14:38:06 +0900

    Feat: 상품 단건 조회, 칵테일 단건 조회시 필요한 dto추가

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/dto/ItemDTO.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/dto/ItemDTO.kt
@@ -1,17 +1,35 @@
 package com.lionTF.CShop.domain.shop.controller.dto
 
+
 import com.lionTF.CShop.domain.admin.models.Category
+import com.lionTF.CShop.global.HttpStatus
+import java.time.temporal.TemporalAmount
 
 
+//상품 단건 조회시 상품 정보를 담기위한 dto
 data class ReadItemDTO (
     val itemId: Long,
-    val price: Int,
-    val amount: Int,
-    val degree: Int,
+    val category: Category,
+    var itemName: String,
+    var price: Int,
+    var totalAmount: Int,
+    var degree: Int,
     var itemDescription: String,
     var itemImgUrl: String,
-    var category: Category,
     var itemStatus: Boolean,
-){
+)
 
-}
+//상품 단건 조회시 최종 응답 형태를 맞춰주기 위한 dto
+data class ReadItemResultDTO(
+    val status: HttpStatus,
+    val message: String,
+    val result: ReadItemDTO
+)
+
+//칵테일 단건 조회시 칵테일에 포함되어 있는 상품들의 정보를 담는 dto
+data class ReadCocktailItemInfoDTO(
+    val itemId: Long,
+    val itemName: String,
+    val price: Int,
+    val amount: Int,
+)
