90be3273341b39a30a9d94bba2a3602f87f9ed7e
chae001122 <chae001122@daum.net>
2022-07-27 14:49:32 +0900

    Feat: 상품 단건 조회 서비스 구현

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/service/ItemService.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/service/ItemService.kt
@@ -1,6 +1,7 @@
 package com.lionTF.CShop.domain.shop.service
 
 import com.lionTF.CShop.domain.shop.controller.dto.ReadItemDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ReadItemResultDTO
 import com.lionTF.CShop.domain.shop.repository.ItemRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,13 +11,15 @@ import java.util.*
 class ItemService (
     private val itemRepository: ItemRepository,
 ){
+    //item 전체 조회 테스트를 위한 메소드
     fun findAllItem(): List<ReadItemDTO> {
         val items = itemRepository.findAll()
         return items.map {it.toReadItemDTO()}
     }
-    
-    fun findByItemId(itemId: Long): Optional<ReadItemDTO>? {
-        val product = itemRepository.findById(itemId)
-        return product.map { it.toReadItemDTO() }
+
+    // 상품 단건 조회를 위한 메소드. 상품 id로 상품 검색
+    fun findByItemId(itemId: Long): ReadItemResultDTO {
+        val item = itemRepository.getReferenceById(itemId).toReadItemDTO()
+        return itemRepository.getReferenceById(itemId).toReadItemResultDTO(item)
     }
 }
