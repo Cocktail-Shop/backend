4b807997acc45de90cfa1c151c5862fbdd80c4ad
chae001122 <chae001122@daum.net>
2022-07-27 14:42:26 +0900

    Feat: Controller에 상품 단건 조회 메소드 구현

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/ItemController.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/ItemController.kt
@@ -1,7 +1,10 @@
 package com.lionTF.CShop.domain.shop.controller
 
 import com.lionTF.CShop.domain.shop.controller.dto.ReadItemDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ReadItemResultDTO
 import com.lionTF.CShop.domain.shop.service.ItemService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.*
@@ -9,11 +12,11 @@ import java.util.*
 import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
-//@RequestMapping("/test")
 class ItemController (
     private val itemService: ItemService
     ){
 
+    // item전체 조회. 테스트를 위한 용도임.
     @GetMapping(path=["/allItem"])
     fun getAllItems(model: Model) : List<ReadItemDTO>{
         val list: List<ReadItemDTO> = itemService.findAllItem()
@@ -21,10 +24,12 @@ class ItemController (
         return itemService.findAllItem()
     }
 
+    //상품 단건 조회
     @GetMapping(path=["/items/{itemId}"])
-    fun getItemById(model: Model, @PathVariable("itemId") itemId: Long): Optional<ReadItemDTO>? {
+    fun getItemById(model: Model, @PathVariable("itemId") itemId: Long): ReadItemResultDTO {
         model.addAttribute("item",itemService.findByItemId(itemId))
         return itemService.findByItemId(itemId)
     }
+
 
 }
