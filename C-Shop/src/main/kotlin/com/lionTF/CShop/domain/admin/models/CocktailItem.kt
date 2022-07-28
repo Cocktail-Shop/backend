2b31dbcc423998cd52c310415fd7d84a7bd1fb65
chae001122 <chae001122@daum.net>
2022-07-28 17:54:39 +0900

    Refactor: 프로퍼티 private 제거, dto변환 메소드 위치 변경

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/models/CocktailItem.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/models/CocktailItem.kt
@@ -1,7 +1,7 @@
 package com.lionTF.CShop.domain.admin.models
 
-import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailItemDTO
-import com.lionTF.CShop.domain.shop.controller.dto.ReadCocktailItemInfoDTO
+import com.lionTF.CShop.domain.shop.controller.dto.CocktailItemDTO
+import com.lionTF.CShop.domain.shop.controller.dto.CocktailItemInfoDTO
 import lombok.*
 import javax.persistence.*
 
@@ -14,25 +14,13 @@ class CocktailItem (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val cocktailItemId: Long,
+    val cocktailItemId: Long,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
-    private var item: Item,
+    var item: Item,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cocktail_id")
-    private var cocktail: Cocktail,
-) {
-
-    //칵테일 단건 조회시, cocktailItems에 들어갈 아이템 정보들을 dto로 변환해주는 메소드
-    fun toReadCocktailItemDTO(): ReadCocktailItemDTO {
-        val item = item.toReadCocktailItemInfoDTO()
-        return ReadCocktailItemDTO(
-            itemId = item.itemId,
-            itemName = item.itemName,
-            price = item.price,
-            amount = item.amount,
-        )
-    }
-}
+    var cocktail: Cocktail,
+)
