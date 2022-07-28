5347db8764443c8452d6b0595d539364e2da4613
chae001122 <chae001122@daum.net>
2022-07-28 17:57:48 +0900

    Refactor : 사용하지 않는 파라미터 및 어노테이션 삭제
    @Mapper annotation제거 및 사용하지 않는 model파라미터 제거

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/ItemController.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/controller/ItemController.kt
@@ -1,14 +1,10 @@
 package com.lionTF.CShop.domain.shop.controller
 
-import com.lionTF.CShop.domain.shop.controller.dto.ReadItemDTO
-import com.lionTF.CShop.domain.shop.controller.dto.ReadItemResultDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ItemDTO
+import com.lionTF.CShop.domain.shop.controller.dto.ItemResultDTO
 import com.lionTF.CShop.domain.shop.service.ItemService
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.*
-import java.util.*
 import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
@@ -17,17 +13,14 @@ class ItemController (
     ){
 
     // item전체 조회. 테스트를 위한 용도임.
-    @GetMapping(path=["/allItem"])
-    fun getAllItems(model: Model) : List<ReadItemDTO>{
-        val list: List<ReadItemDTO> = itemService.findAllItem()
-        model.addAttribute("itemList", list)
-        return itemService.findAllItem()
-    }
+//    @GetMapping(path=["/allItem"])
+//    fun getAllItems(): List<ItemDTO>{
+//        return itemService.findAllItem()
+//    }
 
     //상품 단건 조회
     @GetMapping(path=["/items/{itemId}"])
-    fun getItemById(model: Model, @PathVariable("itemId") itemId: Long): ReadItemResultDTO {
-        model.addAttribute("item",itemService.findByItemId(itemId))
+    fun getItemById(@PathVariable("itemId") itemId: Long): ItemResultDTO {
         return itemService.findByItemId(itemId)
     }
 
--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/repository/CocktailRepository.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/shop/repository/CocktailRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.apache.ibatis.annotations.*
 
-@Mapper
+
 @Repository
 interface CocktailRepository : JpaRepository<Cocktail, Long> {
 
