861758ccfb08ecc4066c59811e82dd656c8cb331
이종수 <81428357+Lee-JongSoo@users.noreply.github.com>
2022-07-30 02:01:45 +0900

    Test: 상품 수정 test 완료 및 수정시 예외상황 테스트 완료

--- C-Shop/src/test/kotlin/com/lionTF/CShop/domain/admin/service/AdminItemServiceImplTest.kt
+++ C-Shop/src/test/kotlin/com/lionTF/CShop/domain/admin/service/AdminItemServiceImplTest.kt
@@ -1,9 +1,8 @@
 package com.lionTF.CShop.domain.admin.service
 
-import com.lionTF.CShop.domain.admin.controller.dto.CreateItemDTO
-import com.lionTF.CShop.domain.admin.controller.dto.setCreateFailItemResultDTO
-import com.lionTF.CShop.domain.admin.controller.dto.setCreateSuccessItemResultDTO
+import com.lionTF.CShop.domain.admin.controller.dto.*
 import com.lionTF.CShop.domain.admin.models.Category
+import com.lionTF.CShop.domain.admin.models.Item
 import com.lionTF.CShop.domain.admin.repository.AdminItemRepository
 import com.lionTF.CShop.domain.admin.service.admininterface.AdminItemService
 import org.assertj.core.api.Assertions
@@ -77,5 +76,80 @@ internal class AdminItemServiceImplTest(
         //then
         assertThat(duplicatedItem.status).isEqualTo(setCreateFailItemResultDTO().status)
         assertThat(duplicatedItem.message).isEqualTo(setCreateFailItemResultDTO().message)
+    }
+
+    @Test
+    @DisplayName("상품 수정 test")
+    fun updateItemTest() {
+        //given
+        var item = Item(
+            itemName = "test",
+            category = Category.ALCOHOL,
+            price = 10000,
+            amount = 10,
+            degree = 20,
+            itemDescription = "상품 설명입니다.",
+            itemStatus = true,
+        )
+
+        val createItem = adminItemRepository.save(item)
+
+        var updateItemDTO = CreateItemDTO(
+            itemName = "test-update",
+            category = Category.ALL,
+            price = 40000,
+            amount = 40,
+            degree = 10,
+            itemDescription = "상품 설명입니다.-update"
+        )
+
+        //when
+        val updateItem = adminItemService.updateItem(createItem.itemId, updateItemDTO)
+
+        val getItem = adminItemRepository.findById(item.itemId).get()
+
+        //then
+        assertThat(updateItem.status).isEqualTo(setUpdateSuccessItemResultDTO().status)
+        assertThat(updateItem.message).isEqualTo(setUpdateSuccessItemResultDTO().message)
+        assertThat(getItem.itemId).isEqualTo(item.itemId)
+        assertThat(getItem.itemName).isEqualTo(updateItemDTO.itemName)
+        assertThat(getItem.category).isEqualTo(updateItemDTO.category)
+        assertThat(getItem.price).isEqualTo(updateItemDTO.price)
+        assertThat(getItem.amount).isEqualTo(updateItemDTO.amount)
+        assertThat(getItem.degree).isEqualTo(updateItemDTO.degree)
+        assertThat(getItem.itemDescription).isEqualTo(updateItemDTO.itemDescription)
+    }
+
+    @Test
+    @DisplayName("상품 수정 예외 test")
+    fun updateItemExceptionTest() {
+        //given
+        var item = Item(
+            itemName = "test",
+            category = Category.ALCOHOL,
+            price = 10000,
+            amount = 10,
+            degree = 20,
+            itemDescription = "상품 설명입니다.",
+            itemStatus = true,
+        )
+
+        val createItem = adminItemRepository.save(item)
+
+        var updateItemDTO = CreateItemDTO(
+            itemName = "test-update",
+            category = Category.ALL,
+            price = 40000,
+            amount = 40,
+            degree = 10,
+            itemDescription = "상품 설명입니다.-update"
+        )
+
+        //when
+        val updateItem = adminItemService.updateItem(3L, updateItemDTO)
+
+        //then
+        assertThat(updateItem.status).isEqualTo(setUpdateFailItemResultDTO().status)
+        assertThat(updateItem.message).isEqualTo(setUpdateFailItemResultDTO().message)
     }
 }
