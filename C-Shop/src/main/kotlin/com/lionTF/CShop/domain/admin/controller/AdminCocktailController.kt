8910c392f1fab4b59bedc9ca202e8280428d8abd
이종수 <81428357+Lee-JongSoo@users.noreply.github.com>
2022-08-10 19:02:38 +0900

    Feat & Test: 한개의 칵테이 삭제 기능 구현

--- C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/controller/AdminCocktailController.kt
+++ C-Shop/src/main/kotlin/com/lionTF/CShop/domain/admin/controller/AdminCocktailController.kt
@@ -64,10 +64,17 @@ class AdminCocktailController(
         return cocktailService.findByCocktailId(cocktailId)
     }
 
-    // 칵테일 삭제
+    // 한개 이상의 칵테일 삭제
     @DeleteMapping("cocktails")
     fun deleteCocktail(@RequestBody deleteCocktailDTO: DeleteCocktailDTO): DeleteCocktailResultDTO {
         return adminCocktailService.deleteCocktail(deleteCocktailDTO)
+    }
+
+    // 한개의 칵테일 삭제
+    @DeleteMapping("cocktails/{cocktailId}")
+    fun deleteOneCocktail(@PathVariable("cocktailId") cocktailId: Long): String {
+        adminCocktailService.deleteOneCocktail(cocktailId)
+        return "admins/cocktail/getAllCocktail"
     }
 
 
